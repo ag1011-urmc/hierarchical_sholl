c507fb74e0633c69ccf1239e665388943f186533
Erik VonKaenel <31524601+vonkaenelerik@users.noreply.github.com>
2023-02-20 09:33:16 -0500

    Update jobArr.sh

--- Code/MCMC_slurm/jobArr.sh
+++ Code/MCMC_slurm/jobArr.sh
@@ -7,8 +7,6 @@
 #SBATCH -a 1-7
 export OMP_NUM_THREADS=4
 
-# This may vary per HPC system. At USC's hpc system
-# we use: source /usr/usc/R/default/setup.sh
 module load r/4.2.1
 
 Rscript --vanilla MDND_Sim_Fit.R
